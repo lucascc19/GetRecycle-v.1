634224144cc535393708bc2cec6afea2c7f45cc7
Lucas Rodrigues Oliveira <Lucas.Oliveira@fitbank.com.br>
2022-12-05 15:29:04 -0300

    tela inicial quase finalizada

--- web/src/App.js
+++ web/src/App.js
@@ -4,26 +4,21 @@ import Home from './components/pages/Home'
 import RestrictedAccess from './components/pages/RestrictedAccess'
 import Map from './components/pages/Map'
 
-import Container from './components/layout/Container'
-import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 
 function App() {
   return (
    <Router>
-    <Navbar />
     <Switch>
-      <Container customClass="min-height">
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/user">
-          <RestrictedAccess />
-        </Route>
-        <Route exact path="/map">
-          <Map />
-        </Route>
-      </Container>  
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/user">
+        <RestrictedAccess />
+      </Route>
+      <Route exact path="/map">
+        <Map />
+      </Route>
     </Switch>
     <Footer />
    </Router>
--- web/src/components/layout/Navbar.js
+++ web/src/components/layout/Navbar.js
@@ -9,14 +9,12 @@ import logo from '../../img/Logo.svg'
 function Navbar() {
   return (
     <nav className={styles.navbar}>
-      <Container>
         <Link to="/">
           <img src={logo} alt="GetRecycle" className={styles.logo} />
         </Link>
         <button className={styles.button}>
           <Link to="/user" >Acesso restrito</Link>
         </button>
-      </Container>
     </nav>
   )
 }
