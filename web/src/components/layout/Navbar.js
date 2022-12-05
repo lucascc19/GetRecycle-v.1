cd8ea8862bda018965851875297c17f2b661cda1
Lucas Oliveira <lucasrodriguesmano@gmail.com>
2022-12-05 10:52:17 -0300

    estilizando a tela inicial da aplicação

--- web/src/components/layout/Navbar.js
+++ web/src/components/layout/Navbar.js
@@ -2,7 +2,8 @@ import { Link } from 'react-router-dom'
 
 import Container from './Container'
 
-import styles from './Container.module.css'
+import styles from './Navbar.module.css'
+
 import logo from '../../img/Logo.svg'
 
 function Navbar() {
@@ -10,13 +11,16 @@ function Navbar() {
     <nav className={styles.navbar}>
       <Container>
         <Link to="/">
-          <img src={logo} alt="GetRecycle" />
+          <img src={logo} alt="GetRecycle" className={styles.logo} />
         </Link>
-        <ul className={styles.list}>
+        <button className={styles.button}>
+          <Link to="/user" >Acesso restrito</Link>
+        </button>
+        {/* <ul className={styles.list}>
           <li>
             <Link to="/user">Acesso restrito</Link>
           </li>
-        </ul>
+        </ul> */}
       </Container>
     </nav>
   )
